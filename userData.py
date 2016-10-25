4e775498322f62786df17df3423fc9f02b2aeaff
swirvinromero <iromerobuild08@gmail.com>
2016-10-24 17:54:47 -0700

    Add files via upload

---
+++ userData.py
@@ -0,0 +1,25 @@
+
+###
+###Product info
+###
+
+wordSearch = "THERMAL LONG"
+colorSearch = "L/S TEE"
+cw = "Ivory"
+cwSize = "Large"
+gold = ""
+
+###
+### User Info
+##
+
+myName = "Kanye West"
+email = "kanye@west.com"
+phoneNumber = "(987)-654-3210"
+address = "123 Fake St Ave"
+zipCode = "98765"
+city = "Palo Alto"
+creditCard = "1223455611112222"
+cvv = "111"
+expMonth = "01"
+expYear = "2020"
